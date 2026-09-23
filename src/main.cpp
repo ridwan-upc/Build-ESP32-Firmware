bc3e6daedf11734a57d26f22036440d8932e5edd
unknown <mohamad.ridwan@upc.edu>
2026-09-23 21:02:45 +0200

    Initial commit: ESP32 CI/CD demo

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    Serial.println("ESP32 is alive!");
+}
